90cc48f99d75ed86b82d6c65504bac5e9eacf93e
katxkelkar <1kelkarkat@gmail.com>
2026-02-01 15:00:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,33 @@ const uploadInp=document.getElementById("uploadInput")
 const captureInp=document.getElementById("captureInput")
 const uploadBtn=document.getElementById("uploadBtn")
 const captureBtn=document.getElementById("captureBtn")
+canvas = document.getElementById("canvas")
+console.log(captureBtn)
 
-function selectImg(){
+async function captureImg(){
     console.log("Image Selection")
-
+    try{
+        stream = await navigator.mediaDevices.getUserMedia({
+            video:{facingMode:"user"},
+            audio:false
+        })
+        preview.srcObject=stream
+        console.log(preview)
+        uploadBtn.disabled=false
+        if (stream){
+            const w = canvas.videoWidth;
+            const h = canvas.videoHeight;
+            canvas.width = w
+            canvas.height = h
+            ctx=canvas.getContext("2d")
+            ctx.drawImage(canvas,0,0,w,h)
+            console.log(canvas,ctx)
+        }
+    }
+    catch(error){
+        console.log(error)
+        
+    }
 }
 
-uploadBtn.addEventListener("click", selectImg)
+captureBtn.addEventListener("click", captureImg)
