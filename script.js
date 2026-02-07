f1a471f7edb4ddfa1c51c7d592d54d99f493f1fe
katxkelkar <1kelkarkat@gmail.com>
2026-02-07 15:10:50 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 console.log("hi")
-const preview=document.getElementById("preview")
+const video=document.getElementById("preview")
 const uploadInp=document.getElementById("uploadInput")
-const captureInp=document.getElementById("captureInput")
+const capturedImg=document.getElementById("capturedImg")
 const uploadBtn=document.getElementById("uploadBtn")
 const captureBtn=document.getElementById("captureBtn")
 canvas = document.getElementById("canvas")
-console.log(captureBtn)
-
+let stream=null
 async function captureImg(){
     console.log("Image Selection")
+    if(stream){
+        captureFrame()
+        return
+    }
     try{
         stream = await navigator.mediaDevices.getUserMedia({
             video:{facingMode:"user"},
             audio:false
         })
-        preview.srcObject=stream
-        console.log(preview)
-        uploadBtn.disabled=false
-        if (stream){
-            const w = canvas.videoWidth;
-            const h = canvas.videoHeight;
-            canvas.width = w
-            canvas.height = h
-            ctx=canvas.getContext("2d")
-            ctx.drawImage(canvas,0,0,w,h)
-            console.log(canvas,ctx)
+        video.srcObject=stream
+        console.log(video)
+        video.onloadedmetadata=()=>{
+                video.play()
+            }
+
         }
-    }
     catch(error){
         console.log(error)
         
     }
 }
+    function captureFrame(){
+        // uploadBtn.disabled=false
+        
+            const w = video.videoWidth;
+            const h = video.videoHeight;
+            canvas.width = w
+            canvas.height = h
+            ctx=canvas.getContext("2d")
+            ctx.drawImage(video,0,0,w,h)
+            // console.log(video,ctx)
+            const dataURL = canvas.toDataURL("image/png")
+            // console.log(dataURL)
+            capturedImg.src=dataURL
+            console.log(capturedImg.src)
+            capturedImg.classList.remove("d-none")
+        
+    }
 
 captureBtn.addEventListener("click", captureImg)
